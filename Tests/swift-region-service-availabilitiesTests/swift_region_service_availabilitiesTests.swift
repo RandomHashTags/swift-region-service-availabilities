7a3e698cd6573f2a3e65ea606133e3e46a2bfd21
RandomHashTags <ImRandomHashTags@gmail.com>
2023-12-03 10:16:28 -0600

    Initial Commit

---
+++ Tests/swift-region-service-availabilitiesTests/swift_region_service_availabilitiesTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_region_service_availabilities
+
+final class swift_region_service_availabilitiesTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
