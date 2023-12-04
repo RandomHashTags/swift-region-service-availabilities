a1f5cd4034915804cf7186826275f5af2a1b1ea5
RandomHashTags <ImRandomHashTags@gmail.com>
2023-12-03 20:30:32 -0600

    software requirement updates

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-region-service-availabilities",
+    defaultLocalization: "en",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v10_15),
+        .iOS(.v12)
     ],
     products: [
         .library(
